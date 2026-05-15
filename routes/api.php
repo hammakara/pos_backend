f781c470e60ba618e666eae01b680f49f4cc1ab8
hammakara <hammakara51@gmail.com>
2026-05-15 20:36:31 +0700

    commited fixed backend endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\API\ProductController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Support\Facades\Route;
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
 
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
